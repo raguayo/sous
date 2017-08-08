cba945c7880675c5d9128271b1228a10e8a0afd4
Willy Raedy <wraedy@gmail.com>
2017-08-08 14:47:41 -0500

    fixed last socket bug

--- server/index.js
+++ server/index.js
@@ -71,9 +71,6 @@ const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 
-  // set up our socket control center
-  const io = socketio(server)
-  require('./socket')(io)
 }
 
 const syncDb = () => db.sync()
