7f25135b9aa56795a6b52960812f9fded1fb2a0a
Willy Raedy <wraedy@gmail.com>
2017-08-12 15:21:50 -0500

    can now parse the quantity and unit from the ingredient

--- extension/ContentScripts/utilityFuncs.js
+++ extension/ContentScripts/utilityFuncs.js
@@ -1,5 +1,5 @@
 // make unit regex
-const unitArr = ['tablespoon', 'teaspoon', 'cup', 'clove', 'package', 'can', 'pound', 'cube', 'bottle', 'pinch', 'square'];
+const unitArr = ['tablespoon', 'teaspoon', 'cup', 'clove', 'package', 'can', 'pound', 'cube', 'bottle', 'pinch', 'square', 'fluid ounce', 'ounce'];
 
 const unitRegex = new RegExp("^(" + unitArr.join("|") + ")(s?)$");
 
@@ -12,57 +12,38 @@ function parseIngredientElements(ingElementArr) {
 
   const ingObjArr = ingSentenceArr.map((sentence) => {
     const wordArr = sentence.split(' ');
-    const quantityArr = [wordArr.shift()];
-    const splitIdx = wordArr.findIndex(word => word.search(unitRegex) !== -1) + 1;
-    const ingObj = {
-      quantity: quantityArr.concat(wordArr.slice(0, splitIdx)).join(' '),
-      name: wordArr.slice(splitIdx).join(' '),
+    let quantity;
+    let unit;
+
+    // find the unit word in the sentence
+    const splitIdx = wordArr.findIndex(word => word.search(unitRegex) !== -1);
+    // split on that unit word to separate the name and the quantity
+    const unitAndQuantityArr = wordArr.slice(0, splitIdx + 1);
+    if (!unitAndQuantityArr.length) {
+      quantity = wordArr.shift();
+    } else {
+      unit = unitAndQuantityArr.pop();
+      quantity = unitAndQuantityArr.join(' ');
     }
+    const name = wordArr.slice(splitIdx + 1).join(' ');
+
+    // check if it has other units in parens e.g. '1 (10 oz) package'
+    const newUnit = quantity.match(/\(.+\)/i)
+    if (newUnit) {
+      quantity = +quantity.slice(0, quantity.indexOf('(') - 1);
+      const newUnitArr = newUnit[0].slice(1, -1).split(' ');
+      quantity *= +newUnitArr[0];
+      unit = newUnitArr.slice(1).join(' ');
+    }
+
+    const ingObj = {
+      quantity,
+      unit,
+      name,
+    };
+
     return ingObj;
-  })
+  });
 
   return ingObjArr;
-  // ingredients.forEach((ingSentence) => {
-  //   const ingQuantity = [];
-  //   const ingName = [];
-  //   const ingWordArr = ingSentence.split(' ');
-
-  //   // find the unit word
-  //   // before that is quantity
-  //   // after that is name
-
-
-
-  //   ingQuantity.push(ingWordArr[0]);
-
-  //   const shouldBeInQuantity = true;
-  //   let index = 1;
-
-  //   while (shouldBeInQuantity) {
-  //     const currentWord = ingWordArr[index];
-  //     if (/^\(.+\)$/i.search(currentWord) !== -1) {
-  //       ingQuantity.push(ingWordArr[index]);
-  //       index += 1;
-  //     } else if (!isNaN(currentWord) || /^\d+\/\d+$/.search(currentWord) !== -1) {
-  //       ingQuantity.push(ingWordArr[index]);
-  //       index += 1;
-  //     } else if () {
-
-  //     } else {
-  //       shouldBeInQuantity = false;
-  //       ingName = ingWordArr.slice(index);
-  //     }
-  //   }
-  // })
-
-  // interate through text
-  // split into words
-  // take first word and put into quantity array
-  // check second word
-    // if it has ( ) around it, put into quantity array
-      // check next index for quantity
-    // if it's a number or a fraction put into quantity arry
-      // check next index for quantity
-    // if it matches a unit put it into quantity array
-  // put the rest into the ingredient arr
 }
