0c7619b391e1f8c33ee0fa940313f638df2f70cb
Willy Raedy <wraedy@gmail.com>
2017-08-12 12:17:19 -0500

    all recipes scraper

---
+++ extension/ContentScripts/cs.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const author = document.getElementsByClassName('submitter__name')[0].textContent;
+  const title = document.getElementsByClassName('recipe-summary__h1')[0].textContent;
+  const recipeUrl = document.URL;
+  const imageUrl = document.getElementsByClassName('rec-photo')[0].currentSrc;
+  const siteName = 'All Recipes';
+  const ingredientElements = document.getElementsByClassName('recipe-ingred_txt');
+  const ingredients = [...ingredientElements].map((ingNode) => {
+    const ingText = ingNode.textContent;
+    if (ingText && ingText !== 'Add all ingredients to list') return ingText;
+    return null;
+  }).filter(el => !!el);
+
+  const servingsEl = document.querySelector('[ng-bind="adjustedServings"]');
+  const observer = new MutationObserver((mutations) => {
+    const numServings = +document.querySelector('[ng-bind="adjustedServings"]').textContent;
+    const recipeObj = {
+      title, author, recipeUrl, imageUrl, siteName, numServings
+    }
+    chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+      sendResponse({ recipe: recipeObj, ingredients });
+    })
+  });
+  const config = { attributes: true, childList: true, characterData: true };
+  observer.observe(servingsEl, config);
+})
