8f48430e5c0b185919a57c6fbb831942ffe578ae
Rick Aguayo <rick.aguayo@gmail.com>
2017-08-17 12:40:22 -0500

    added a little style to chrome extension

--- extension/Popup/popup.js
+++ extension/Popup/popup.js
@@ -8,11 +8,11 @@ const styles = {
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const tabId = tabs[0].id;
   chrome.tabs.sendMessage(tabId, { greeting: 'Yo!' }, (response) => {
-    let htmlString = `
+    let htmlString = `<div>
       <h3 style="margin: 0.857143em 0.857143em">Recipe Details:</h3>
       <table class="ui table" style="table-layout: fixed">
         <tr>
-          <td>Title: &emsp;${response.recipe.title}</td>
+          <td >Title: &emsp;${response.recipe.title}</td>
         </tr>
         <tr>
           <td>Author: &emsp;${response.recipe.author}</td>
@@ -22,24 +22,29 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         </tr>
       </table>
       <h3 style="margin: 0.857143em 0.857143em">Ingredients:</h3>
-      <div class="ui grid" style="margin: 0.857143em 0.857143em">
-        <div class="row" style="margin: 0.857143em 0.857143em">
-          <div class="six wide column"><p>Ingredient Name</p></div>
-          <div class="four wide column"><p>Quantity</p></div>
-          <div class="four wide column"><p>Unit</p></div>
-        </div>`;
+      <table class="ui grid altrowstable" id="alternatecolor" style="margin: 0.857143em 0.857143em; box-shadow: 1px 1px 1px #999;">
+        <thead style="display:table-header-group">
+          <tr>
+            <th style="padding:1em; width='100%';align:'center';"><p>Ingredient Name</p></th>
+            <th style="padding:1em; width='100%'; align:'center';"><p>Quantity</p></th>
+            <th style="padding:1em; width='100%'; align:'center';"><p>Unit</p></th>
+          </tr>
+        </thead>
+        <tbody>`;
 
     response.ingredients.forEach((ingObj) => {
-      const ingHTML = `<div class="row" style="margin: 0.857143em 0.857143em">
-          <div class="six wide column"><p>${ingObj.name}</p></div>
-          <div class="four wide column"><p>${ingObj.quantity}</p></div>
-          <div class="four wide column"><p>${ingObj.unit}</p></div>
-        </div>`;
+      const ingHTML = `<tr style="margin: 0.857143em 0.857143em">
+          <td style="padding:1em; width='100%'; align='center';"><p>${ingObj.name}</p></td>
+          <td style="padding:1em; width='100%'; align='center';"><p>${ingObj.quantity}</p></td>
+          <td style="padding:1em; width='100%'; align='center';"><p>${ingObj.unit}</p></td>
+        </tr>`;
       htmlString += ingHTML;
     });
 
+    htmlString += '</tbody></table>';
+
     // const buttonHtml = '<button type"button">Send Recipe to Sous</button>';
-    const buttonHtml = '<button style="margin: 0.857143em 0.857143em" type="button" id="btnSendRecipe" name="btnSendRecipe">Send Recipe to Sous</button></div>';
+    const buttonHtml = '<button style="margin: 0.857143em 0.857143em" type="button" id="btnSendRecipe" name="btnSendRecipe">Send Recipe to Sous</button></table>';
     htmlString += buttonHtml;
 
     htmlString += '</div>';
@@ -50,5 +55,9 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       response.msg = 'createGroceryList';
       chrome.extension.sendMessage(response);
     });
+
+    // $(document).ready(() => {
+    //   $('tr:odd').css('background-color', '#E5E7E9');
+    // });
   });
 });
