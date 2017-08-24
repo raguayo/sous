8f2cdfd6f433242c8efa9cd857c3bd47627522f3
Rick Aguayo <rick.aguayo@gmail.com>
2017-08-24 13:45:01 -0500

    updated user modals so they close now

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -33,12 +33,18 @@ class UserProfile extends Component {
   constructor() {
     super();
     this.state = {
-      modalOpen: false,
+      nameModalOpen: false,
+      emailModalOpen: false,
+      passwordModalOpen: false,
     };
   }
 
-  handleClose = () => this.setState({ modalOpen: false });
-  handleOpen = () => this.setState({ modalOpen: true });
+  handleNameModalClose = () => this.setState({ nameModalOpen: false });
+  handleNameModalOpen = () => this.setState({ nameModalOpen: true });
+  handleEmailModalClose = () => this.setState({ emailModalOpen: false });
+  handleEmailModalOpen = () => this.setState({ emailModalOpen: true });
+  handlePasswordModalClose = () => this.setState({ passwordModalOpen: false });
+  handlePasswordModalOpen = () => this.setState({ passwordModalOpen: true });
 
   render() {
     const { user, handleNameUpdate, handleEmailUpdate, handlePasswordUpdate } = this.props;
@@ -57,6 +63,7 @@ class UserProfile extends Component {
               <Segment stacked>
                 <Grid>
                   <Grid.Column floated="left" width={13} verticalAlign="middle">
+
                     <Form.Input
                       name="name"
                       value={user.name}
@@ -69,18 +76,20 @@ class UserProfile extends Component {
                   </Grid.Column>
                   <Grid.Column floated="right" width={3} textAlign="left">
                     <Modal
-                      trigger={<Icon onClick={this.handleOpen} name="edit" />}
-                      open={this.state.modalOpen}
-                      onClose={this.handleClose}
+                      trigger={<Icon onClick={this.handleNameModalOpen} name="edit" />}
+                      open={this.state.nameModalOpen}
+                      onClose={this.handleNameModalClose}
                       basic size="small"
                     >
                       <Modal.Content>
-                        <Form onSubmit={e => handleNameUpdate(e, user)}>
+                        <Form
+                          size="small"
+                          onSubmit={e => handleNameUpdate(e, user, this.handleNameModalClose)}
+                        >
                           <Input
                             label="Edit Name:"
                             name="name"
-                            onClick={this.handleClose}
-                            action={{ color: 'gray', labelPosition: 'left', icon: 'add', content: 'Submit' }}
+                            action={{ style: { backgroundColor: '#77a95f', color: 'white' }, labelPosition: 'left', icon: 'add', content: 'Submit' }}
                           />
                         </Form>
                       </Modal.Content>
@@ -89,7 +98,7 @@ class UserProfile extends Component {
                 </Grid>
                 <Grid>
                   <Grid.Column floated="left" width={13} verticalAlign="middle">
-                    <Form.Input
+                    <Input
                       name="email"
                       value={user.email}
                       readOnly
@@ -101,17 +110,20 @@ class UserProfile extends Component {
                   </Grid.Column>
                   <Grid.Column floated="right" width={3} textAlign="left">
                     <Modal
-                      trigger={<Icon onClick={this.handleOpen} name="edit" />}
-                      open={this.state.modalOpen}
-                      onClose={this.handleClose}
+                      trigger={<Icon onClick={this.handleEmailModalOpen} name="edit" />}
+                      open={this.state.emailModalOpen}
+                      onClose={this.handleEmailModalClose}
                       basic size="small"
                     >
                       <Modal.Content>
-                        <Form onSubmit={e => handleEmailUpdate(e, user)}>
+                        <Form
+                          size="small"
+                          onSubmit={e => handleEmailUpdate(e, user, this.handleEmailModalClose)}
+                        >
                           <Input
                             label="Edit Email:"
                             name="email"
-                            action={{ color: 'teal', labelPosition: 'left', icon: 'add', content: 'Submit' }}
+                            action={{ style: { backgroundColor: '#77a95f', color: 'white' }, labelPosition: 'left', icon: 'add', content: 'Submit' }}
                           />
                         </Form>
                       </Modal.Content>
@@ -132,26 +144,28 @@ class UserProfile extends Component {
                     />
                   </Grid.Column>
                   <Grid.Column floated="right" width={3} textAlign="left">
-                    {/* <Modal
-                      trigger={<Icon onClick={this.handleOpen} name="edit" />}
-                      open={this.state.modalOpen}
-                      onClose={this.handleClose}
+                    <Modal
+                      trigger={<Icon onClick={this.handlePasswordModalOpen} name="edit" />}
+                      open={this.state.passwordModalOpen}
+                      onClose={this.handlePasswordModalClose}
                       basic size="small"
                     >
                       <Modal.Content>
-                        <Form onSubmit={e => handlePasswordUpdate(e, user)}>
+                        <Form
+                          onSubmit={e => handlePasswordUpdate(e, user, this.handlePasswordModalClose)}
+                          size="small"
+                        >
+
                           <Input
                             label="Edit Password:"
                             name="password"
-                            action={{ color: 'teal', labelPosition: 'left', icon: 'add', content: 'Submit' }}
+                            action={{ style: { backgroundColor: '#77a95f', color: 'white' }, labelPosition: 'left', icon: 'add', content: 'Submit' }}
                           />
-
                         </Form>
                       </Modal.Content>
-                    </Modal> */}
+                    </Modal>
                   </Grid.Column>
                 </Grid>
-
               </Segment>
             </Form>
           </Grid.Column>
@@ -167,24 +181,26 @@ class UserProfile extends Component {
 const mapState = (state) => {
   return {
     user: state.user,
-    modalOpen: false,
   };
 };
 
-const mapDispatch = (dispatch, ownProps) => {
+const mapDispatch = (dispatch) => {
   return {
-    handleNameUpdate(e, user) {
-      // console.log('ownProps: ', ownProps);
+    handleNameUpdate(e, user, handleNameModalClose) {
+      console.log('in handleNameUpdate mapDispatch')
       const objUser = { id: user.id, name: e.target.name.value };
       dispatch(editUserName(objUser));
+      handleNameModalClose();
     },
-    handleEmailUpdate(e, user) {
+    handleEmailUpdate(e, user, handleEmailModalClose) {
       const objUser = { id: user.id, email: e.target.email.value };
       dispatch(editUserEmail(objUser));
+      handleEmailModalClose();
     },
-    handlePasswordUpdate(e, user) {
+    handlePasswordUpdate(e, user, handlePasswordModalClose) {
       const objUser = { id: user.id, password: e.target.password.value };
       dispatch(editUserPassword(objUser));
+      handlePasswordModalClose();
     },
   };
 };
