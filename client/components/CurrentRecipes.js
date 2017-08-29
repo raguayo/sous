8a45d441b2e7a7c4ffd20efadf10600f4b434218
Willy Raedy <wraedy@gmail.com>
2017-08-29 14:30:43 -0500

    user response for url bar

--- client/components/CurrentRecipes.js
+++ client/components/CurrentRecipes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Container, Grid, Header, Segment, Input, Form } from 'semantic-ui-react';
+import { Container, Grid, Header, Segment, Input, Form, Loader, Dimmer } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { deleteRecipesFromList, postNewRecipe } from '../store';
 import { EmptyList } from './';
 import CurrRecipeCard from './CurrRecipeCard';
 
 const styles = {
+  loader: {
+    height: '5em',
+  },
   recipeCol: {
     padding: '1em 1em 1em 1em',
     display: 'flex',
@@ -31,69 +34,91 @@ const styles = {
   },
   topMarg: {
     paddingTop: '3em',
-  }
+  },
 };
 
-function CurrentRecipe({
-  handleAddRecipe,
-  handleDeleteRecipes,
-  groceryListRecipes,
-  user,
-}) {
-  return (
-    <Container style={styles.container}>
-      <Grid textAlign="center">
-        <Grid.Row>
-          <Header as="h1" style={styles.header}>Welcome, {user.name}</Header>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={12}>
-            <Grid.Row>
-              <Form onSubmit={handleAddRecipe}>
-                <Input
-                  name="recipeUrl"
-                  style={styles.recipeInput}
-                  action={{
-                    labelPosition: 'left',
-                    icon: 'add',
-                    content: 'Add',
-                    style: { backgroundColor: '#77a95f', color: 'white' }
-                  }}
-                  placeholder="Recipe url..."
-                />
-              </Form>
-            </Grid.Row>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      {groceryListRecipes.length ?
-        <Segment.Group>
-          <Segment>
-            <p style={styles.color}>Your currently selected recipes:</p>
-          </Segment>
-          <Grid relaxed padded stretched>
-            {
-              groceryListRecipes && groceryListRecipes.map((currRecipe) => {
-                return (
-                  <Grid.Column width={5} style={styles.recipeCol} key={currRecipe.id}>
-                    <CurrRecipeCard recipe={currRecipe} />
-                  </Grid.Column>
-                );
-              },
-              )}
-          </Grid>
-          <Segment>
-            <Link to={'./grocery-list'} className="appButton">View Your Grocery List!</Link>
-            <a
-              onClick={() => handleDeleteRecipes()}
-              className="appButton"
-            > Clear Recipe List</a>
-          </Segment>
-        </Segment.Group> :
-        <EmptyList style={styles.topMarg} />
-      }
-    </Container>
-  );
+class CurrentRecipe extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayLoader: false,
+    };
+  }
+
+  displayLoader = () => {
+    console.log('In display loader')
+    this.setState({ displayLoader: true })
+  }
+  hideLoader = () => this.setState({ displayLoader: false })
+
+  render() {
+    console.log(this.state.displayLoader)
+    const { handleAddRecipe, handleDeleteRecipes, groceryListRecipes, user } = this.props;
+    return (
+      <Container style={styles.container}>
+        <Grid textAlign="center">
+          <Grid.Row>
+            <Header as="h1" style={styles.header}>Welcome, {user.name}</Header>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              <Grid.Row>
+                <Form onSubmit={e => handleAddRecipe(e, this.displayLoader, this.hideLoader)}>
+                  <Input
+                    name="recipeUrl"
+                    style={styles.recipeInput}
+                    action={{
+                      labelPosition: 'left',
+                      icon: 'add',
+                      content: 'Add',
+                      style: { backgroundColor: '#77a95f', color: 'white' }
+                    }}
+                    placeholder="Recipe url..."
+                  />
+                </Form>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        {
+          this.state.displayLoader ? <Segment style={styles.loader}>
+              <Dimmer active inverted>
+            <Loader size='mini'>
+                Loading
+            </Loader>
+              </Dimmer>
+          </Segment> : null
+        }
+        {groceryListRecipes.length ?
+          <Segment.Group>
+            <Segment>
+              <p style={styles.color}>Your currently selected recipes:</p>
+            </Segment>
+            <Grid relaxed padded stretched>
+              {
+                groceryListRecipes && groceryListRecipes.map((currRecipe) => {
+                  return (
+                    <Grid.Column width={5} style={styles.recipeCol} key={currRecipe.id}>
+                      <CurrRecipeCard recipe={currRecipe} />
+                    </Grid.Column>
+                  );
+                },
+                )}
+            </Grid>
+            <Segment>
+              <Link to={'./grocery-list'} className="appButton">View Your Grocery List!</Link>
+              <a
+                onClick={() => handleDeleteRecipes()}
+                className="appButton"
+              > Clear Recipe List</a>
+            </Segment>
+          </Segment.Group> :
+          <EmptyList style={styles.topMarg} />
+        }
+      </Container>
+    );
+  }
 }
 
 const mapState = (state) => {
@@ -105,9 +130,12 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleAddRecipe: e => {
+    handleAddRecipe: (e, showLoader, hideLoader) => {
       e.preventDefault();
-      dispatch(postNewRecipe(e.target.recipeUrl.value, true));
+      showLoader();
+      dispatch(postNewRecipe(e.target.recipeUrl.value, true))
+      .then(() => hideLoader())
+      .catch(console.error);
     },
     handleDeleteRecipes: () => {
       dispatch(deleteRecipesFromList());
