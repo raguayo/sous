b6407780e2b0982a08a7d111c8e2ea2c92191e03
Willy Raedy <wraedy@gmail.com>
2017-08-30 19:50:21 -0500

    styled the splash page

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,50 +1,50 @@
 import React from 'react';
-import { Segment, Container, Grid, Header, Image } from 'semantic-ui-react';
+import { Segment, Header, Image } from 'semantic-ui-react';
 
 const styles = {
+  photo: {
+    marginRight: '0.5em',
+  },
+  madeBy: {
+    marginRight: '1em',
+    marginBottom: '0em',
+    fontFamily: 'Roboto-Slab',
+  },
   footer: {
-    backgroundColor: '#84643B',
+    backgroundColor: '#0c0c0c',
     verticalAlign: 'center',
   },
-  logo: {
-    fontFamily: 'Oleo Script Swash Caps',
-    color: '#77a95f',
-    fontSize: '3rem',
-    margin: '0 3rem',
-    fontWeight: '300',
-    paddingBottom: '2.5rem',
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0em 2em',
+  },
+  photoContainer: {
+    display: 'flex',
+    alignItems: 'center',
   },
 };
 
 const Footer = () => (
   <Segment inverted vertical style={styles.footer}>
-      <Grid inverted stackable>
-        <Grid.Row>
-          <Grid.Column width={3} style={{ paddingTop: '1.25rem' }}>
-            <a href="https://github.com/raguayo/raww">
-              <Image src="/images/GitHub-Logo.png" size="small" centered />
-            </a>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <a href="https://www.linkedin.com/in/william-raedy-13a161147/">
-              <Image src="/images/WillyRaedy.jpeg" size="tiny" shape="circular" centered />
-              <Header inverted as="h4" content="Willy Raedy" textAlign="center" />
-            </a>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <a href="https://www.linkedin.com/in/alexander-j-stauter-7239ba93/">
-              <Image src="/images/AlexStauter.jpeg" size="tiny" shape="circular" centered />
-              <Header inverted as="h4" content="Alex Stauter" textAlign="center" />
-            </a>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <a href="https://www.linkedin.com/in/rick-aguayo/">
-              <Image src="/images/RickAguayo.jpeg" size="tiny" shape="circular" centered />
-              <Header inverted as="h4" content="Rick Aguayo" textAlign="center" />
-            </a>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+    <div style={styles.container}>
+      <a href="https://github.com/raguayo/raww" target="_blank" rel="noopener noreferrer">
+        <Image src="http://git-class.gr/images/GitHub_Logo.png" size="small" centered />
+      </a>
+      <div style={styles.photoContainer}>
+        <Header inverted as="h2" content="Made By: " textAlign="left" style={styles.madeBy} />
+        <a href="https://www.linkedin.com/in/william-raedy-13a161147/" style={styles.photo} target="_blank" rel="noopener noreferrer">
+          <Image src="/images/WillyRaedy.jpg" size="tiny" shape="circular" centered />
+        </a>
+        <a href="https://www.linkedin.com/in/alexander-j-stauter-7239ba93/" style={styles.photo} target="_blank" rel="noopener noreferrer">
+          <Image src="/images/AlexStauter.jpeg" size="tiny" shape="circular" centered />
+        </a>
+        <a href="https://www.linkedin.com/in/rick-aguayo/" style={styles.photo} target="_blank" rel="noopener noreferrer">
+          <Image src="/images/RickAguayo.jpeg" size="tiny" shape="circular" centered />
+        </a>
+      </div>
+    </div>
   </Segment>
 );
 
