00dc705d5478088636895b1b8291a7f35433b957
Willy Raedy <wraedy@gmail.com>
2017-08-30 19:04:11 -0500

    new footer color

--- client/components/footer.js
+++ client/components/footer.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Segment, Container, Grid, Header, Image } from 'semantic-ui-react';
 
 const styles = {
+  footer: {
+    backgroundColor: '#84643B',
+    verticalAlign: 'center',
+  },
   logo: {
     fontFamily: 'Oleo Script Swash Caps',
     color: '#77a95f',
@@ -13,42 +17,34 @@ const styles = {
 };
 
 const Footer = () => (
-  <Segment inverted vertical>
-    <Container>
+  <Segment inverted vertical style={styles.footer}>
       <Grid inverted stackable>
         <Grid.Row>
           <Grid.Column width={3} style={{ paddingTop: '1.25rem' }}>
-            <a href="http://drsous.herokuapp.com/recipes">
-              <div style={styles.logo}>
-                <Image src="/images/chefHatNoBackground.jpeg" size="small" shape="circular" centered />
-                sous
-              </div>
-            </a>
             <a href="https://github.com/raguayo/raww">
-              <Image src="/images/blueGithub.png" size="mini" shape="circular" centered />
+              <Image src="/images/GitHub-Logo.png" size="small" centered />
             </a>
           </Grid.Column>
           <Grid.Column width={4}>
             <a href="https://www.linkedin.com/in/william-raedy-13a161147/">
-              <Image src="/images/WillyRaedy.jpeg" size="small" shape="circular" centered />
+              <Image src="/images/WillyRaedy.jpeg" size="tiny" shape="circular" centered />
               <Header inverted as="h4" content="Willy Raedy" textAlign="center" />
             </a>
           </Grid.Column>
           <Grid.Column width={4}>
             <a href="https://www.linkedin.com/in/alexander-j-stauter-7239ba93/">
-              <Image src="/images/AlexStauter.jpeg" size="small" shape="circular" centered />
+              <Image src="/images/AlexStauter.jpeg" size="tiny" shape="circular" centered />
               <Header inverted as="h4" content="Alex Stauter" textAlign="center" />
             </a>
           </Grid.Column>
           <Grid.Column width={4}>
             <a href="https://www.linkedin.com/in/rick-aguayo/">
-              <Image src="/images/RickAguayo.jpeg" size="small" shape="circular" centered />
+              <Image src="/images/RickAguayo.jpeg" size="tiny" shape="circular" centered />
               <Header inverted as="h4" content="Rick Aguayo" textAlign="center" />
             </a>
           </Grid.Column>
         </Grid.Row>
       </Grid>
-    </Container>
   </Segment>
 );
 
