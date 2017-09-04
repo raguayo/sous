1d51cd17d92f80d0e591a9de84e4add4aa96a7fd
Willy Raedy <wraedy@gmail.com>
2017-09-04 12:40:55 -0500

    refactored to handle trying to add a duplicate recipe

--- extension/Popup/popup.js
+++ extension/Popup/popup.js
@@ -1,5 +1,27 @@
 /* global chrome $ */
 
+function handlePostResponse(response) {
+  $('#submit-loader').hide();
+  let htmlString = '<div class="ui message result-message">';
+  if (response.groceryListRecipe) htmlString += '<h5>Your recipe was added!</h5>';
+  else if (response.savedRecipe) htmlString += '<h5>Your recipe was saved!</h5>';
+  else if (response.responseText === 'Validation error') htmlString += '<h5>You\'ve already added this recipe. Go to your profile page on Sous to add it to your grocery list.</h5>';
+  else htmlString += '<h5>Well, this is embarassing. Something went wrong when trying to add your recipe!</h5>';
+  htmlString += '</div>';
+  $('#ingredients-table').after(htmlString);
+}
+
+function submitRecipe(recipe, inGroceryList) {
+  $('#ingredients-table').after(`<div class="ui segment" id="submit-loader">
+        <div class="ui active inverted dimmer">
+          <div class="ui text loader">Loading</div>
+        </div>
+      </div>`);
+  chrome.extension.sendMessage({ msg: 'createGroceryList', recipe, inGroceryList }, (response) => {
+    handlePostResponse(response);
+  });
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const recipeUrl = tabs[0].url;
   chrome.runtime.sendMessage({ recipeUrl, msg: 'getRecipeDetails' }, (recipe) => {
@@ -77,12 +99,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     htmlString += '</tbody></table>';
 
     const buttonHtml = `<div id="button-footer">
-    <div class="ui buttons my-button-wrapper">
-      <button class="ui button my-button" id="saveBtn">Save Recipe</button>
-      <div class="or"></div>
-      <button class="ui button my-button" id="glistBtn">Add It To Your Grocery List</button>
-    </div>
-  </div>`;
+      <div class="ui buttons my-button-wrapper">
+        <button class="ui button my-button" id="saveBtn">Save Recipe</button>
+        <div class="or"></div>
+        <button class="ui button my-button" id="glistBtn">Add It To Your Grocery List</button>
+      </div>
+    </div>`;
 
 
     htmlString += buttonHtml;
@@ -91,29 +113,11 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     $('#wrapper').append(htmlString);
 
     $('#saveBtn').click(() => {
-      $('#ingredients-table').after(`<div class="ui segment" id="submit-loader">
-              <div class="ui active inverted dimmer">
-                <div class="ui text loader">Loading</div>
-              </div>
-            </div>`);
-      chrome.extension.sendMessage({ msg: 'createGroceryList', recipe, inGroceryList: false }, (response) => {
-        $('#submit-loader').hide();
-        if (response.savedRecipe) $('#ingredients-table').after('<div class="ui message result-message"><h5>Your recipe was saved!</h5></div>');
-        else $('#ingredients-table').after('<div class="ui message result-message"><h5>Well, this is embarassing. Something went wrong when trying to save your recipe!</h5></div>');
-      });
+      submitRecipe(recipe, false);
     });
 
     $('#glistBtn').click(() => {
-      $('#ingredients-table').after(`<div class="ui segment" id="submit-loader">
-        <div class="ui active inverted dimmer">
-          <div class="ui text loader">Loading</div>
-        </div>
-      </div>`);
-      chrome.extension.sendMessage({ msg: 'createGroceryList', recipe, inGroceryList: true }, (response) => {
-        $('#submit-loader').hide();
-        if (response.groceryListRecipe) $('#ingredients-table').after('<div class="ui message result-message"><h5>Your recipe was added!</h5></div>');
-        else $('#ingredients-table').after('<div class="ui message result-message"><h5>Well, this is embarassing. Something went wrong when trying to add your recipe!</h5></div>');
-      });
+      submitRecipe(recipe, true);
     });
   });
 });
