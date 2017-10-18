5c3a8a6e0b4a59f1dc8706905e23b13bdd0eec1c
Willy Raedy <wraedy@gmail.com>
2017-10-18 17:17:56 -0500

    fix error handling in saved recipe reducer

--- client/store/savedRecipes.js
+++ client/store/savedRecipes.js
@@ -32,7 +32,7 @@ export const fetchSavedRecipes = () =>
       .then((recipes) => {
         dispatch(getSavedRecipes(recipes));
       })
-      .catch(addError);
+      .catch(err => dispatch(addError(err)));
 
 export const deleteSavedRecipe = recipeId =>
   dispatch =>
@@ -42,7 +42,7 @@ export const deleteSavedRecipe = recipeId =>
         dispatch(removeSavedRecipe(recipeId));
         dispatch(removeRecipeFromList(recipeId));
       })
-      .catch(addError);
+      .catch(err => dispatch(addError(err)));
 
 export const postNewRecipe = (url, inGroceryList) =>
   (dispatch) => {
@@ -55,7 +55,7 @@ export const postNewRecipe = (url, inGroceryList) =>
         }
         dispatch(getSavedRecipes(recipes.savedRecipe));
       })
-      .catch(addError);
+      .catch(err => dispatch(addError(err)));
   };
 
 export const favoriteToggle = recipeId => dispatch =>
@@ -64,7 +64,7 @@ export const favoriteToggle = recipeId => dispatch =>
     .then((toggled) => {
       dispatch(toggleFavoriteRecipe(toggled));
     })
-    .catch(addError);
+    .catch(err => dispatch(addError(err)));
 /**
  * REDUCER
  */
